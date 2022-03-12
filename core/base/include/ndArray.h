eab7f63cae01e4d27cb9fec6b3834a1139bb41e6
AsakusaRinne <AsakusaRinne@gmail.com>
2022-03-12 13:36:46 +0800

    fix(core/base/include): fix memory leak of NDArray.
    fix the memory leak caused by default deconstructor of NDArray

--- core/base/include/ndArray.h
+++ core/base/include/ndArray.h
@@ -1,11 +1,71 @@
 #pragma once
 
+#include <memory>
+
 #include "core/base/include/layout.h"
 
 namespace nncore {
+
+class RefPtr {
+  std::shared_ptr<void *> m_ref;
+  size_t m_offset;
+  bool m_mutable;
+
+ public:
+  RefPtr() {
+    m_ref = std::make_shared<void *>((void *)nullptr);
+    m_offset = 0;
+    m_mutable = true;
+  }
+
+  RefPtr(void *ref_ptr, const size_t offset = 0) {
+    m_ref = std::make_shared<void *>(ref_ptr);
+    m_offset = offset;
+    m_mutable = true;
+  }
+
+  explicit RefPtr(std::shared_ptr<void *> ref_ptr, const size_t offset = 0,
+                  bool is_mutable = true) {
+    m_ref = ref_ptr;
+    m_offset = offset;
+    m_mutable = is_mutable;
+  }
+
+  void *get_ptr() const {
+    return static_cast<void *>(
+        (*m_ref != NULL) ? static_cast<nn_byte *>(*m_ref) + m_offset : nullptr);
+  }
+
+  bool is_mutable() const { return m_mutable; }
+
+  void reset(const void *ptr, size_t offset = 0);
+
+  RefPtr &operator+=(size_t offset) {
+    m_offset += offset;
+    return *this;
+  }
+
+  bool operator==(const RefPtr &other) const {
+    return *m_ref == *other.m_ref && m_offset == other.m_offset;
+  }
+
+  template <typename T>
+  T *ptr() const {
+    return static_cast<T *>(get_ptr());
+  }
+
+  void alloc_memory(const Shape &shape, const DType &dtype) {
+    *m_ref = malloc(shape.count() * dtype.size());
+  }
+
+  ~RefPtr() {
+    free(*m_ref);
+    *m_ref = nullptr;
+  }
+};
+
 struct NDArray {
   Layout layout;
-  void *raw_ptr;
 
   /*
    * \brief This method should be always used on an empty array!
@@ -14,19 +74,23 @@ struct NDArray {
    */
   void relayout(const Layout &layout) {
     this->layout = layout;
-    raw_ptr = alloc_memory(layout, layout.dtype);
+    m_ref_ptr.alloc_memory(layout, layout.dtype);
   }
 
-  NDArray() : raw_ptr(nullptr) {}
+  NDArray() : m_ref_ptr(RefPtr((void *)nullptr)) {}
 
   NDArray(const Layout &layout)
-      : layout(layout), raw_ptr(alloc_memory(layout, layout.dtype)) {}
+      : layout(layout), m_ref_ptr(RefPtr((void *)nullptr)) {
+    m_ref_ptr.alloc_memory(layout, layout.dtype);
+  }
 
   NDArray(const Shape &shape, const DType dtype)
-      : layout(shape, dtype), raw_ptr(alloc_memory(shape, dtype)) {}
+      : layout(shape, dtype), m_ref_ptr(RefPtr((void *)nullptr)) {
+    m_ref_ptr.alloc_memory(layout, layout.dtype);
+  }
 
   NDArray(const Layout &layout_, void *raw_ptr_)
-      : layout(layout_), raw_ptr(raw_ptr_) {}
+      : layout(layout_), m_ref_ptr(raw_ptr_) {}
 
   /*!
    * \brief Get the value by the indcies of dims.
@@ -51,7 +115,7 @@ struct NDArray {
   template <typename T>
   T *ptr() const {
     layout.dtype.assert_is_ctype<T>();
-    return static_cast<T *>(raw_ptr);
+    return static_cast<T *>(m_ref_ptr.get_ptr());
   }
 
   template <typename T>
@@ -75,8 +139,6 @@ struct NDArray {
   }
 
  private:
-  void *alloc_memory(const Shape &shape, const DType &dtype) {
-    return malloc(shape.count() * dtype.size());
-  }
+  RefPtr m_ref_ptr;
 };
 }  // namespace nncore
