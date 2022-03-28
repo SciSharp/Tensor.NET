fe2b3264f28d7f6ba566985844247940758ac640
AsakusaRinne <AsakusaRinne@gmail.com>
2022-03-29 01:16:04 +0800

    feat(csharp/): refactor the project structure of csharp part.

--- csharp/Num.NET/Program.cs
+++ csharp/Num.NET/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Runtime.InteropServices;
-using System.Buffers;
+﻿using System.Runtime.InteropServices;
 using Numnet.Native;
 using Numnet.Tensor;
 using Numnet.Tensor.Utilities;
@@ -9,8 +7,6 @@ unsafe{
     Tensor<int> a = new Tensor<int>(new int[]{1, 2, 3, 4, 5, 6}, new int[] { 2, 3 });
     Tensor<double> b = new Tensor<double>(new double[]{1.2, 2.6, 3.9, 4.1, 5.0, 6.5, 1.7, 2, 3, 4, 5, 6}, new int[] { 3, 4 });
     Tensor<double> c = Tensor.Zeros<double>(new int[] { 2, 4 });
-    // b.AsSpan().Fill(1);
-    // b.AsSpan()[1] = 2;
     IntPtr status = Tensor<int>.Execute(a, b, c, NativeApi.Matmul, IntPtr.Zero, Provider.Naive);
     for (int i = 0; i < c.AsSpan().Length; i++){
         Console.Write(c.AsSpan()[i] + " ");
@@ -18,11 +14,4 @@ unsafe{
     Console.WriteLine();
     Console.WriteLine(NativeStatus.GetErrorCode(status));
     Console.WriteLine(NativeStatus.GetErrorMessage(status));
-}
-
-
-[StructLayout(LayoutKind.Sequential)]
-struct Shape{
-    public IntPtr shape;
-    public ulong ndim;
 }
