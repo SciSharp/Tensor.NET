dc82ff4e423b839771e82e26c2fb182fd5b992a8
AsakusaRinne <AsakusaRinne@gmail.com>
2022-04-21 11:41:49 +0800

    test(csharp/TensorBasicTest): add test for slice.

---
+++ csharp/TensorBasicTest/Slice.cs
@@ -0,0 +1,49 @@
+using NUnit.Framework;
+using Numnet;
+using System;
+using System.Collections.Generic;
+
+namespace TensorBasicTest;
+
+public class SliceTest
+{
+    private List<Tuple<Tensor, Tensor>> _map;
+    [SetUp]
+    public void Setup()
+    {
+        _map = new List<Tuple<Tensor, Tensor>>(new Tuple<Tensor, Tensor>[]{
+            new Tuple<Tensor, Tensor>(Tensor.FromArray(new int[]{1, 2, 3, 4, 5, 6}, new int[]{2, 3}), 
+                        Tensor.FromArray(new int[]{1, 2, 3, 4, 5, 6}, new int[]{3, 2})),
+            new Tuple<Tensor, Tensor>(
+                Tensor.FromArray(new int[]{1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6}, new int[]{3, 8}), 
+                Tensor.FromArray(new int[]{1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6}, new int[]{2, 2, 6}))
+        });
+    }
+
+    [Test]
+    public void Test1()
+    {
+        var src = Tensor.FromArray(new int[] { 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6 }, new int[] { 3, 8 });
+        var dst = src[new Slice(1, 3), new Slice(3, 6)];
+        Tensor truth = Tensor.FromArray(new int[] { 6, 1, 2, 2, 3, 4 }, new int[] { 2, 3 });
+        Assert.IsTrue(TensorUtils.IsValueEqual(dst, truth));
+    }
+
+    [Test]
+    public void Test2()
+    {
+        var src = Tensor.FromArray(new int[] { 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6 }, new int[] { 3, 8 });
+        var dst = src[new Slice(0, 3, 2), new Slice(1, 8, 3)];
+        Tensor truth = Tensor.FromArray(new int[] { 2, 5, 2, 6, 3, 6 }, new int[] { 2, 3 });
+        Assert.IsTrue(TensorUtils.IsValueEqual(dst, truth));
+    }
+
+    [Test]
+    public void Test3()
+    {
+        var src = Tensor.FromArray(new int[] { 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6 }, new int[] { 3, 8 });
+        var dst = src[new Slice(1, 3), new Slice(5)];
+        Tensor truth = Tensor.FromArray(new int[] { 2, 4 }, new int[] { 2 });
+        Assert.IsTrue(TensorUtils.IsValueEqual(dst, truth));
+    }
+}
