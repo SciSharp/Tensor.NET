dd2544fc041316c28c98cefc63388fc330813791
AsakusaRinne <AsakusaRinne@gmail.com>
2022-04-27 01:37:33 +0800

    feat(csharp/Num.NET/Math/BasicFunction): add abs method.

---
+++ csharp/Num.NET/Math/BasicFunction/Abs.cs
@@ -0,0 +1,21 @@
+using Numnet.Common;
+
+namespace Numnet.Math{
+    public static class AbsExtension{
+        public static Tensor<double> Add(this Tensor<double> inp){
+            return OnElemOperation.Execute<double, double>(inp, x => System.Math.Abs(x));
+        }
+        public static Tensor<float> Add(this Tensor<float> inp){
+            return OnElemOperation.Execute<float, float>(inp, x => System.Math.Abs(x));
+        }
+        public static Tensor<long> Add(this Tensor<long> inp){
+            return OnElemOperation.Execute<long, long>(inp, x => System.Math.Abs(x));
+        }
+        public static Tensor<int> Add(this Tensor<int> inp){
+            return OnElemOperation.Execute<int, int>(inp, x => System.Math.Abs(x));
+        }
+        public static Tensor<bool> Add(this Tensor<bool> inp){
+            return OnElemOperation.Execute<bool, bool>(inp, x => x);
+        }
+    }
+}
