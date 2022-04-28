0113134fb611951a0a597e45a42838f875dabd87
AsakusaRinne <AsakusaRinne@gmail.com>
2022-04-29 01:19:49 +0800

    fix(csharp/Num.NET/Random): remove default parameter value for NormalInternal.

--- csharp/Num.NET/Random/Normal.cs
+++ csharp/Num.NET/Random/Normal.cs
@@ -9,7 +9,7 @@ namespace Numnet{
             NormalInternal<T>(res, mean, std);
             return res;
         }
-        private unsafe static void NormalInternal<T>(Tensor<T> t, double mean = .0, double std = .1) where T : struct, IConvertible, IEquatable<T>{
+        private unsafe static void NormalInternal<T>(Tensor<T> t, double mean, double std) where T : struct, IConvertible, IEquatable<T>{
             NormalParam param = new NormalParam() { mean = mean, std = std };
             IntPtr status = NativeExecutor.Execute(NativeApi.Normal, t.TMemory, t.TLayout, new IntPtr(&param), Tensor<T>.Provider);
             NativeStatus.AssertOK(status);
